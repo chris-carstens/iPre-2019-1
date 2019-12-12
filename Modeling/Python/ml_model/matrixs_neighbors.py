aaea78d974704f9ad99f226a732f080275553e32
Mauro S. Mendoza Elguera <msmendoza@uc.cl>
2019-12-12 09:59:44 -0300

    MM

--- Modeling/Python/ml_model/matrixs_neighbors.py
+++ Modeling/Python/ml_model/matrixs_neighbors.py
@@ -2,19 +2,24 @@ import numpy as np
 
 from scipy.signal import convolve, convolve2d
 
-D = np.random.randint(low=0, high=10, size=(10, 10), dtype=int)
+D = np.random.randint(low=0, high=10, size=(3, 3), dtype=int)
 
 
 def li_neighbors(matrix, i=1):
     """
-    Calcula la cantidad de incidentes en las celdas vecinas de la
-    i-ésima capa
+    Calcula la cantidad de incidentes en la i-ésima capa (capa tipo
+    ProMap).
 
-    @param matrix:
-    @param i:
-    @return:
+    :type matrix: np.ndarray
+    :param matrix:
+    :param i: int que indica la i-ésima capa a calcular
+    :return: ndarray con la suma de incidentes de la i-ésima capa en
+    cada celda
     """
 
-    ker = np.ones(shape=matrix.shap)
+    ker = np.ones(shape=matrix.shape, dtype=int)
 
-    return convolve2d()
+    return convolve2d(matrix, ker, mode='same')
+
+
+print(D, li_neighbors(D) - D, sep='\n' * 2)
