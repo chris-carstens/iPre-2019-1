fc9dfe1fe695ae91ae6771fd5f784d8422639966
msmendoza <msmendoza@uc.cl>
2020-05-18 23:49:28 -0400

    Fixing Repo

---
+++ predictivehp/models/models.py
@@ -0,0 +1,25 @@
+"""
+models.py
+Python Version: 3.8.1
+
+iPre - Big Data para Criminología
+Created by Mauro S. Mendoza Elguera at 18-05-20
+Pontifical Catholic University of Chile
+
+"""
+
+
+class STKDE():
+    pass
+
+
+class RForestRegressor():
+    pass
+
+
+class ProMap():
+    pass
+
+
+if __name__ == '__main__':
+    pass
