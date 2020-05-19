0797e45024c0a2a6bb6ec5dc6f7dd181e9b02b14
msmendoza <msmendoza@uc.cl>
2020-05-18 22:38:27 -0400

    Fixing Repo

---
+++ predictivehp/processing/__init__.py
@@ -0,0 +1,13 @@
+"""
+__init__.py
+Python Version: 3.8.1
+
+iPre - Big Data para Criminología
+Created by Mauro S. Mendoza Elguera at 03-05-20
+Pontifical Catholic University of Chile
+
+"""
+
+if __name__ == '__main__':
+    pass
+
