d468f77f3b3ca778bd9a03275d7ac393d0b15528
msmendoza <msmendoza@uc.cl>
2020-05-27 10:27:19 -0400

    Reu. 20/05 update

---
+++ predictivehp/visualization/plotter.py
@@ -0,0 +1,33 @@
+"""
+plotter.py
+Python Version: 3.8.1
+
+iPre - Big Data para Criminología
+Created by Mauro S. Mendoza Elguera at 20-05-20
+Pontifical Catholic University of Chile
+
+"""
+
+
+class Plotter:
+    def __init__(self, models):
+        """
+
+        :param list models: Lista con los objetos de los diferentes
+            modelos. e.g. [stkde, rfr, pm]
+        """
+
+        self.models = models
+
+    def heatmap(self):
+        pass
+
+    def hr(self):
+        pass
+
+    def pai(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
