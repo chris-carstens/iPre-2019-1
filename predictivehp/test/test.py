49511e1f8c939af7c500b4e6475beca4d5712936
msmendoza <msmendoza@uc.cl>
2020-06-03 00:55:44 -0400

    testing

--- predictivehp/test/test.py
+++ predictivehp/test/test.py
@@ -8,17 +8,23 @@ Pontifical Catholic University of Chile
 
 """
 
-import numpy as np
 import geopandas as gpd
+import matplotlib.pyplot as plt
+
 from shapely.geometry import Point
+from pyproj import Proj, transform
 
 if __name__ == '__main__':
-    # source        EPSG: 900913 (WGS84) (*)
+    # source  WGS84 EPSG: 4326 (WGS84) (*) 900913
     # destination   EPSG: 3857
 
     ans = gpd.GeoDataFrame(
-        geometry=[Point((32.643838, -96.999343))],
-        crs='epsg:900913'
+        geometry=[Point((-96.798645, 32.742206))],
+        crs=4326,
     )
     ans.to_crs(epsg=3857, inplace=True)
-    # s_shp = gpd.read_file('./../data/streets.shp')
+
+    # P3857 = Proj(init='epsg:3857')
+    # P4326 = Proj(init='epsg:4326')
+    #
+    # x, y = transform(P4326, P3857, 32.742206, -96.798645)
