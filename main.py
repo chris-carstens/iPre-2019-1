4c6c3ff155966179d7f46370816a0c7335efab55
msmendoza <msmendoza@uc.cl>
2020-07-07 22:31:26 -0400

    Updates on stkde.py

--- main.py
+++ main.py
@@ -24,26 +24,26 @@ df, shps['streets'], shps['councils'], shps['c_limits'] = \
 # stkde = STKDE(df=df)
 
 # %% Random Forest Regressor
-#rfr = RForestRegressor(i_df=df, shps=shps,
-                       #xc_size=100, yc_size=100, layers_n=7,
-                       #read_data=False, read_df=False)
-#rfr.heatmap()
+rfr = RForestRegressor(i_df=df, shps=shps,
+                       xc_size=100, yc_size=100, layers_n=7,
+                       read_data=False, read_df=False)
+# rfr.heatmap()
 
 # rfr.to_pickle('data.pkl')
 # rfr.to_pickle('df.pkl')
 
 # %%
-pm = ProMap(i_df=df, bw=bw, read_files=False)
-pm.heatmap(c=0)
+# pm = ProMap(i_df=df, bw=bw, read_files=False)
+# pm.heatmap(c=0)
 
 # %% Plotter
-pltr = Plotter(models=[
-#     stkde,
+# pltr = Plotter(models=[
+# #     stkde,
 #     rfr,
-     pm
-  ])
+#      # pm
+#   ])
 
-pltr.pai()
+# pltr.pai()
 
 if __name__ == '__main__':
     # TODO Reu. 20/05
