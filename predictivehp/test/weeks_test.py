06643c480ff217a8fdfed1e864f0a7cdbb3f4560
msmendoza <msmendoza@uc.cl>
2020-07-08 15:34:46 -0400

    Reu. 08/07

--- predictivehp/test/weeks_test.py
+++ predictivehp/test/weeks_test.py
@@ -7,8 +7,16 @@ from predictivehp.processing.data_processing import get_data
 # 4 weeks train: 3 features, 1 label
 # para predecir: hay desplazamiento en una semana.
 
-df, _, _, _ = get_data()
 n_weeks = 3
+
+weeks = []
+for n in range(n_weeks):
+    try:
+        date(2017, 10, 18 - n)
+    except Exception as err:
+        pass
+
+df, _, _, _ = get_data()
 
 weeks = [f"W {date(2017, 10, 18 - i)}" for i in [0, 7, 14][::-1]]
 layers = [f"Layer_{i}" for i in range(8)]
